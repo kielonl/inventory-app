3211da4c80f86270378a7ca35810a020bed4c6ba
kielon <mosadowski23@gmail.com>
2023-01-11 13:29:13 +0100

    turned task board into table

--- src/components/Board/components/ItemBoard.vue
+++ src/components/Board/components/ItemBoard.vue
@@ -10,24 +10,43 @@
     <div class="board-add-item-button">
       <IconButton @click="showCreateModal()" :icon="'➕'" />
     </div>
-
-    <div class="items-wrapper">
-      <div v-if="state.items.length === 0" class="no-items">
-        No items received
-      </div>
-      <div
-        v-if="state.items.length"
-        v-for="item in state.items"
-        :key="item.uuid"
-        class="items-container"
-      >
-        <ItemBox
-          :type="item.type"
-          :name="item.name"
-          :uuid="item.uuid"
-          :save="save"
-          :showEditModal="showEditModal"
-        />
+    <div class="table-center">
+      <div class="items-wrapper">
+        <div class="header-row">
+          <div class="items-table-cell items-cell-lp">Lp.</div>
+          <div class="items-other-cell-wrapper">
+            <div class="items-table-cell name">Name</div>
+            <div class="items-table-cell type">Type</div>
+            <div class="items-table-cell description">Description</div>
+          </div>
+        </div>
+        <div class="items-table-row" v-for="(item, index) in state.items">
+          <div class="items-table-cell items-cell-lp">{{ index }}</div>
+          <div class="items-other-cell-wrapper">
+            <div class="items-table-cell name">{{ item.name }}</div>
+            <div class="items-table-cell type">{{ item.type }}</div>
+            <div class="items-table-cell description cut-text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit id
+              quam maiores animi inventore nisi quis. Eos laborum harum corrupti
+              consequatur quod aliquid quis incidunt tenetur impedit eaque
+              ducimus, ad vero nulla repellendus maiores rem fugit minima odio
+              architecto illum rerum? Fugit provident totam, minima neque eos
+              dolorum enim consequatur.
+            </div>
+            <div class="items-table-cell edit-remove">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path
+                  d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"
+                />
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path
+                  d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -44,16 +63,43 @@ import * as ItemService from "../../../services/itemService";
 import type { Item, ItemError } from "../../../types";
 
 const state = reactive<any>({
-  items: [],
+  categories: ["jeden", "dwa", "trzy", "cztery", "pienc"],
+  items: [
+    {
+      type: "jeden",
+      name: "halo",
+      uuid: "1",
+    },
+    {
+      type: "jeden",
+      name: "halo",
+      uuid: "1",
+    },
+    {
+      type: "trzy",
+      name: "halo",
+      uuid: "1",
+    },
+    {
+      type: "cztery",
+      name: "halo",
+      uuid: "1",
+    },
+    {
+      type: "pienc",
+      name: "halo",
+      uuid: "1",
+    },
+  ],
 });
 
-onMounted(async () => {
-  const result = await ItemService.read();
-  if (!result) {
-    return setError("Unknown error");
-  }
-  state.items = result;
-});
+// onMounted(async () => {
+//   const result = await ItemService.read();
+//   if (!result) {
+//     return setError("Unknown error");
+//   }
+//   state.items = result;
+// });
 
 const item = ref<Item>({
   type: "",
@@ -115,6 +161,26 @@ const setError = (errorMessage: string = "Unknown error") => {
 const findItemIndex = (id: string | undefined): number => {
   return state.items.findIndex((obj: any) => obj.uuid == id);
 };
+
+const findItemType = (val: string) =>
+  state.items.reduce(
+    (acc: any, el: any, i: any) => (el.type === val ? [...acc, i] : acc),
+    []
+  );
+
+// function findItemType(type: string): any[] {
+//   var indexes = [];
+//   var indices = state.items
+//     .map((e: any, i: any) => (e === type ? i : ""))
+//     .filter(String);
+//   return indices;
+//   i = -1;
+// while ((i = state.items.indexOf(type, i + 1)) != -1) {
+//   console.log(i);
+//   indexes.push(i);
+
+// return indexes;
+// }
 
 const createItem = async (): Promise<void> => {
   const result = await ItemService.write({
