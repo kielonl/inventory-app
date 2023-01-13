5d852b74d9a89c3b25b7cc5ee2c1d8f21ea8e8ea
kielon <mosadowski23@gmail.com>
2023-01-13 11:06:49 +0100

    moved router to its own file

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-import { createRouter, useRouter, createWebHistory } from "vue-router";
+import router from "./plugins/router";
 
 import "./assets/main.scss";
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "Login",
-      component: () => import("./components/Login/components/Login.vue"),
-    },
-    {
-      path: "/home",
-      name: "Home",
-      component: () => import("./components/Board/components/ItemBoard.vue"),
-    },
-  ],
-});
 
 const pinia = createPinia();
 const app = createApp(App);
