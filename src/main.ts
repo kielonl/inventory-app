ce9d2f7b217828d071a9c7d669a13d53122fb0ba
kielon <mosadowski23@gmail.com>
2023-01-13 10:46:15 +0100

    resolved conflicts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import { createRouter, useRouter, createWebHistory } from "vue-router";
 
+import "./assets/main.scss";
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -19,12 +21,9 @@ const router = createRouter({
   ],
 });
 
-const app = createApp(App);
-
-import "./assets/main.scss";
-
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
+app.use(router);
 app.mount("#app");
