84ee9bc5b726438ae22cbc7fb1bd3c394378d051
kielon <mosadowski23@gmail.com>
2023-01-13 10:14:13 +0100

    testStore now stores empty object

--- src/stores/Test.ts
+++ src/stores/Test.ts
@@ -2,12 +2,7 @@ import { defineStore } from "pinia";
 import { ref, computed } from "vue";
 
 export const useTestStore = defineStore("testStore", () => {
-  const obj = ref({ pozdrawiam: "Huberta Dziude" });
-  const getObj = computed(() => "Pozdrawiam " + obj.value.pozdrawiam);
+  const obj = ref({});
 
-  const changeObj = (name: string) => {
-    obj.value.pozdrawiam = name;
-  };
-
-  return { obj, getObj, changeObj };
+  return { obj };
 });
